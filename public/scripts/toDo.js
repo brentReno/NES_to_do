715a7755aa82077aa86b575ab5641e4104a3a63e
Brent Reno <brent.reno@gmail.com>
2016-09-17 11:21:05 -0500

    add update status route, and is functioning

--- public/scripts/toDo.js
+++ public/scripts/toDo.js
@@ -17,10 +17,27 @@ $(document).ready(function(){
       url:"/newTask",
       data: objectToSend,
       success: function(data){
-        console.log("back with:", data);  
-      }
-
+        console.log("back with:", data);
+      }//end success
     });//end AJAX call
   });//END CREATE TASK CLICK
 
-});
+  //update status on CLICK
+  $("#changeTaskStaus").on('click', function(){
+    console.log("In chnageTaskStatus on click");
+    //create test object
+    var objectToSend ={
+      status: true
+    };
+    //AJAX call
+    $.ajax({
+      type:"POST",
+      url: "/changeStatus",
+      data: objectToSend,
+      success: function(data){
+      console.log("back from changeStatus:", data);
+    }// end success
+
+    });//end ajax
+  });// end change task on click
+});//end doc ready
--- server/app.js
+++ server/app.js
@@ -17,10 +17,19 @@ app.get("/", function(req,res){
   res.sendFile(path.resolve("public/index.html"));
 });
 
+//post route to create new task
 app.post("/newTask", urlEncodedParser, function(req ,res){
   console.log("creating a new task");
   res.send("Hello from New Task");
 });
+
+//post route to change status
+app.post("/changeStatus", urlEncodedParser, function(req, res){
+  console.log("changeing a task");
+  res.send("Hello from Change Staus");
+});
+
+//delete route to remove task
 
 
 //set public folder as static
