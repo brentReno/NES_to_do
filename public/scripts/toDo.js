84f7dafdf12812a4bb8c2be17cfc1e820860f989
Brent Reno <brent.reno@gmail.com>
2016-09-18 15:42:01 -0500

    Added CSS and error handeling for create task

--- public/scripts/toDo.js
+++ public/scripts/toDo.js
@@ -2,36 +2,46 @@ console.log("toDo script is sourced");
 
 $(document).ready(function(){
   console.log("JQ is sourced");
+  //set focus to taskIn
+  $('#taskIn').focus();
   //call display tasks
   displayTask();
-
   //Create Task on Click
   $('#createNewTask').on('click', function(){
     console.log("In createNewTask Click");
-    //create true false values for task
-    var statusIn;
-    if($('#statusIn').val() == "Complete"){
-      statusIn = true;
-    }
-    else if($('#statusIn').val()=="Work In Progress"){
-      statusIn = false;
-    } //end if else
-    // assemble object to send
-    var taskToCreate ={
-      task:$('#taskIn').val(),
-      status: statusIn
-    };
-    console.log("task to create:", taskToCreate);
-    //AJAX call to server
-    $.ajax({
-      type:"POST",
-      url:"/newTask",
-      data: taskToCreate,
-      success: function(data){
-        console.log("back with:", data);
-      }//end success
-    });//end AJAX call
+    //check for empty field
+    if($('#taskIn').val()===""){
+      $('#taskIn').fadeOut("slow").fadeIn("slow").focus();
+    }else{
+      //create true false values for task
+      var statusIn;
+      if($('#statusIn').val() == "Complete"){
+        statusIn = true;
+      }
+      else if($('#statusIn').val()=="Work In Progress"){
+        statusIn = false;
+      }
+      else if($('#statusIn').val()!= "Complete" ||$('#statusIn').val()!= "Work In Progress" ){
+        $('#statusIn').fadeOut().fadeIn();
+        return;
+      }//end if else
+      // assemble object to send
+      var taskToCreate ={
+        task:$('#taskIn').val(),
+        status: statusIn
+      };
+      console.log("task to create:", taskToCreate);
+      //AJAX call to server
+      $.ajax({
+        type:"POST",
+        url:"/newTask",
+        data: taskToCreate,
+        success: function(data){
+          console.log("back with:", data);
+        }//end success
+      });//end AJAX call
       displayTask();
+    }//end else
   });//END CREATE TASK CLICK
 
   //update status on CLICK
@@ -59,15 +69,16 @@ $(document).ready(function(){
       data: objectToSend,
       success: function(data){
       console.log("back from changeStatus:", data);
-      displayTask();
+
     }// end success
 
     });//end ajax
+    displayTask();
   });// end change task on click
 
   //Delete Safety check
   $("#deleteTask").on("click", function(){
-    $("#deleteTaskDiv").append("<div id='safetyCheck'><p> Are you sure you want to delet this Task?</p><button id='deleteYes'>Delete</button><button id='deleteNo'>Don't Delete</button></div>");
+    $("#deleteTaskDiv").append("<div id='safetyCheck'><p> Are you sure you want to delete this Task?</p><button id='deleteYes'>Delete</button><button id='deleteNo'>Don't Delete</button></div>");
   });//end delete safety check
 
   //Delete Yes on click
@@ -105,13 +116,17 @@ var displayTask = function(){
       console.log(" back with the Tasks:", data);
       //establish display string
       var displayString="";
-      var selectList="";
-      var deleteList="";
+      var selectList="<option value=''disabled selected>Select a Task</option>";
+      var deleteList="<option value=''disabled selected>Select a Task</option>";
       //loop through data array
       for (var i = 0; i < data.length; i++) {
+        if(data[ i ].status === true){
+        displayString +='<li class="complete" id ="'+data[ i ].id+'"data-value="'+ data[ i ].status+'">'+ data[ i ].task +"</li>";
+      } else{
         displayString +='<li id ="'+data[ i ].id+'"data-value="'+ data[ i ].status+'">'+ data[ i ].task +"</li>";
-        selectList+='<option data-value="'+data[ i ].id+'"> Item '+ data[ i ].id + "</option>";
-        deleteList+='<option data-value="'+data[ i ].id+'"> Item '+ data[ i ].id + "</option>";
+      }
+        selectList+='<option data-value="'+data[ i ].id+'">'+ data[ i ].task + "</option>";
+        deleteList+='<option data-value="'+data[ i ].id+'">'+ data[ i ].task + "</option>";
       }
       //display on DOM
       $('#toDoList').html(displayString);
